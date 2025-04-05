38d50090a843a48f23e5eb45b1a18b34a3863edb
YoGurt <islamdautov59@gmail.com>
2025-04-06 01:05:40 +0500

    Add project files.

---
+++ AdPlatform/Controllers/PlatfromsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace AdPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatfromsController : ControllerBase
+    {
+        // GET: api/<PlatfromsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PlatfromsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PlatfromsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PlatfromsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PlatfromsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
